154a4cf1aa5ba11bb98478b7550c2a3e6e2c84ee
badrsh980 <badrsh980@gmail.com>
2023-12-29 05:38:11 +0300

    bader

--- netmasha/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ netmasha/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\badrs\Desktop\Capstone\New folder\netmasha"
+export "FLUTTER_APPLICATION_PATH=C:\Users\badrs\Desktop\Capstone\NETMASHA\netmasha"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.1.0"
