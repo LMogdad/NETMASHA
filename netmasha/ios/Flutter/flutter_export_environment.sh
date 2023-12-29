4e9f8f64b124f661971e85ae0473be183475c084
badrsh980 <badrsh980@gmail.com>
2023-12-29 05:32:18 +0300

    bader

--- netmasha/ios/Flutter/flutter_export_environment.sh
+++ netmasha/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter\flutter_windows_3.0.1-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\zezoo\OneDrive\Desktop\NETMASHA\netmasha"
+export "FLUTTER_ROOT=D:\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\badrs\Desktop\Capstone\New folder\netmasha"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- netmasha/lib/main.dart
+++ netmasha/lib/main.dart
@@ -1,12 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:netmasha/blocs/auth_bloc/auth_bloc.dart';
-import 'package:netmasha/blocs/onbaording_bloc/onbaording_bloc.dart';
-import 'package:netmasha/screens/nav_bar.dart';
-import 'package:netmasha/screens/splash_screen.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MainApp());
@@ -15,49 +7,12 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  Future<bool> _checkIfLoggedIn() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    String token = prefs.getString("token") ?? "";
-    return token.isNotEmpty;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).requestFocus(FocusNode());
-      },
-      child: MultiBlocProvider(
-        providers: [
-          BlocProvider<OnbaordingBloc>(
-              create: (BuildContext context) => OnbaordingBloc()),
-          BlocProvider<AuthBloc>(create: (BuildContext context) => AuthBloc()),
-          // Add other BlocProviders if needed
-        ],
-        child: MaterialApp(
-          theme: ThemeData(fontFamily: "IBM Plex Sans Arabic"),
-          locale: const Locale('ar'),
-          localizationsDelegates: const [
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [Locale('ar')],
-          debugShowCheckedModeBanner: false,
-          home: FutureBuilder<bool>(
-            future: _checkIfLoggedIn(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.data == true) {
-                  return NavBar();
-                } else {
-                  return const SplashScreen();
-                }
-              } else {
-                return const CircularProgressIndicator();
-              }
-            },
-          ),
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
         ),
       ),
     );
